613f4aea3c2ad405ef475583dad6054f440e3de7
AleksanderPirojok <sanchous.n23@gmail.com>
2024-01-22 20:56:28 +0100

    fix: fix a bug in the case with removed files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,14 +1,20 @@
-const { mkdir, rm, readdir, copyFile } = require('fs/promises');
-// const { access } = require('fs');
+const { mkdir, readdir, copyFile, unlink } = require('fs/promises');
 const path = require('path');
 
 async function copyDir() {
   const dirPath = path.join(__dirname, 'files');
   const copyDirPath = path.join(__dirname, 'files-copy');
   const files = readdir(dirPath, { withFileTypes: true });
-  //  access(copyDirPath);
-  // rm(copyDirPath, { recursive: true });
-  mkdir(copyDirPath, { recursive: true });
+
+  await mkdir(copyDirPath, { recursive: true });
+  const copiedFiles = readdir(copyDirPath, { withFileTypes: true });
+
+  await copiedFiles.then((filesArr) => {
+    filesArr.forEach((file) => {
+      const newFilePath = path.join(copyDirPath, file.name);
+      unlink(newFilePath);
+    });
+  });
 
   files.then((filesArr) => {
     filesArr.forEach((file) => {
